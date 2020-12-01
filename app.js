f39b95e320df65408172f0568aa75e8dae5e4080
seehearcait <caitlinmichie19@gmail.com>
2020-11-30 18:46:29 -0600

    Testing HTML options

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-import { modules } from './js/queenName.js';
-import { modules as _modules } from './js/challenge.js';
-import { getCatchphrase } from './js/catchphrase.js';
-import ordinal from 'ordinal';
+// import { modules } from './js/queenName.js';
+// import { modules as _modules } from './js/challenge.js';
+// import { getCatchphrase } from './js/catchphrase.js';
+// import ordinal from 'ordinal';
 
-// ** DOM ELEMENTS
-const button = document.getElementById('button')
-const message = document.getElementById('message')
-const test = document.getElementById('test');
+const queenName = require('./js/queenName.js');
+const challenge = require('./js/challenge.js');
+const phrase = require('./js/catchphrase.js');
+const ordinal = require('ordinal');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+app.use(cors());
+
+// // ** DOM ELEMENTS
+// const button = document.getElementById('button')
+// const message = document.getElementById('message')
+// const test = document.getElementById('test');
+
+// // ** CONSTRUCT FIRST NAME
+// const firstName = modules.getFirstName();
+// const lastName = modules.getLastName();
+// const fullName = firstName + " " + lastName;
 
 // ** CONSTRUCT FIRST NAME
-const firstName = modules.getFirstName();
-const lastName = modules.getLastName();
+const firstName = queenName.modules.getFirstName();
+const lastName = queenName.modules.getLastName();
 const fullName = firstName + " " + lastName;
 
+// // ** FIND PLACEMENT AND CHALLENGE PERFORMANCE
+// const placement = _modules.finalPlacement();
+// const challengeWin = _modules.winningChallenge(placement);
+// const challengeLoss = _modules.losingChallenge(placement, challengeWin);
+
 // ** FIND PLACEMENT AND CHALLENGE PERFORMANCE
-const placement = _modules.finalPlacement();
-const challengeWin = _modules.winningChallenge(placement);
-const challengeLoss = _modules.losingChallenge(placement, challengeWin);
+const placement = challenge.modules.finalPlacement()
+const challengeWin = challenge.modules.winningChallenge(placement);
+const challengeLoss = challenge.modules.losingChallenge(placement, challengeWin);
 
 // ** GET CATCHPHRASE
-const catchPhrase =  getCatchphrase();
+const catchPhrase =  phrase.getCatchphrase();
 
 const yourProfile = {
     name: fullName,
@@ -29,13 +51,13 @@ const yourProfile = {
     catchPhrase: catchPhrase
 };
 
-function displayMessage(profile){
-    // console.log('Ran DM')
+function displayMessage(){
     test.innerHTML = 'Ran!'
-    // message.innerHTML = profile;
-}
+    message.innerHTML = yourProfile;
+};
 
 console.log(yourProfile);
 
-//Button On Click
-button.addEventListener('click', displayMessage(yourProfile));
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
